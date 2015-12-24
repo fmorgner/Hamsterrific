457f460b67a15f8a7407f77053fb69b0f0aa93a4
Felix Morgner <felix.morgner@gmail.com>
2015-12-24 22:03:47 +0100

    Use correct way to scale

--- src/main/java/es/razzleberri/hamsterrific/renderer/RenderHamster.java
+++ src/main/java/es/razzleberri/hamsterrific/renderer/RenderHamster.java
@@ -3,14 +3,13 @@ package es.razzleberri.hamsterrific.renderer;
 import es.razzleberri.hamsterrific.Hamsterrific;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.model.ModelBase;
+import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.entity.RenderLiving;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.util.ResourceLocation;
-import org.lwjgl.opengl.GL11;
 
 public class RenderHamster extends RenderLiving {
-
 
   public RenderHamster(ModelBase modelbaseIn, float shadowsizeIn) {
     super(Minecraft.getMinecraft().getRenderManager(), modelbaseIn, shadowsizeIn);
@@ -18,11 +17,12 @@ public class RenderHamster extends RenderLiving {
 
   @Override
   protected void preRenderCallback(EntityLivingBase entitylivingbaseIn, float partialTickTime) {
-    GL11.glScalef(.5f, .5f, .5f);
+    GlStateManager.scale(.5, .5, .5);
   }
 
   @Override
   protected ResourceLocation getEntityTexture(Entity entity) {
     return new ResourceLocation(Hamsterrific.MOD_ID + ":textures/entity/hamster/darkbrown.png");
   }
+
 }
