164d253d4a1a2f7b7dd6b4ce3417298e0e78c9cd
Felix Morgner <felix.morgner@gmail.com>
2015-12-25 16:15:36 +0100

    Add basic food consumption

--- src/main/java/es/razzleberri/hamsterrific/entity/EntityHamster.java
+++ src/main/java/es/razzleberri/hamsterrific/entity/EntityHamster.java
@@ -15,6 +15,9 @@ import java.util.List;
 public abstract class EntityHamster extends EntityTameable {
 
   private final List<Integer> m_breedingItems;
+  private final List<Integer> m_foodItems;
+
+  private int m_foodLevel = 0;
 
   public EntityHamster(World world) {
     super(world);
@@ -26,10 +29,47 @@ public abstract class EntityHamster extends EntityTameable {
     tasks.addTask(0, new EntityAIWatchClosest(this, EntityPlayer.class, 80.0f));
 
     m_breedingItems = new ArrayList<>();
+    m_foodItems = new ArrayList<>();
+  }
+
+  public int getFoodLevel() {
+    return m_foodLevel;
   }
 
   protected final void addBreedingItem(Item item) {
     m_breedingItems.add(Item.getIdFromItem(item));
+  }
+
+  protected final void addFoodItem(Item item) {
+    m_foodItems.add(Item.getIdFromItem(item));
+  }
+
+  protected final void consumeFood() {
+    if(m_foodLevel > 0) {
+      --m_foodLevel;
+    }
+  }
+
+  protected abstract void updateFoodLevel();
+
+  private boolean handleEatableItem(EntityPlayer player) {
+    ItemStack heldItem = player.getHeldItem();
+
+    if(m_foodLevel < 4) {
+      consumeItemFromStack(player, heldItem);
+
+      if(++m_foodLevel == 4 && isBreedingItem(heldItem)) {
+        setInLove(player);
+      }
+
+      return true;
+    }
+
+    return false;
+  }
+
+  private boolean isEatable(ItemStack stack) {
+    return isBreedingItem(stack) || m_foodItems.contains(Item.getIdFromItem(stack.getItem()));
   }
 
   @Override
@@ -55,6 +95,22 @@ public abstract class EntityHamster extends EntityTameable {
   @Override
   protected String getDeathSound() {
     return Hamsterrific.MOD_ID + ":deathSound";
+  }
+
+  @Override
+  public boolean interact(EntityPlayer player) {
+    if(isEatable(player.getHeldItem())) {
+      return handleEatableItem(player);
+    }
+
+    return false;
+  }
+
+  @Override
+  public void onLivingUpdate() {
+    updateFoodLevel();
+
+    super.onLivingUpdate();
   }
 
 }
--- src/main/java/es/razzleberri/hamsterrific/entity/EntityRegularHamster.java
+++ src/main/java/es/razzleberri/hamsterrific/entity/EntityRegularHamster.java
@@ -5,9 +5,14 @@ import net.minecraft.init.Items;
 import net.minecraft.world.World;
 
 public class EntityRegularHamster extends EntityHamster {
+
   public EntityRegularHamster(World world) {
     super(world);
+
     addBreedingItem(Items.pumpkin_seeds);
+
+    addFoodItem(Items.wheat_seeds);
+    addFoodItem(Items.melon_seeds);
   }
 
   @Override
@@ -15,4 +20,10 @@ public class EntityRegularHamster extends EntityHamster {
     return null;
   }
 
+  @Override
+  protected void updateFoodLevel() {
+    if(rand.nextInt(42) == 0 && rand.nextInt(42) == 0) {
+      consumeFood();
+    }
+  }
 }
